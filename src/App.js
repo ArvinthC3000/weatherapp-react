6c468d03a1a00385f0c7e743fcb3c1739be7f88f
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-03 20:28:28 +0530

    Including sub components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Form from './components/Form'
+import Title from './components/Title'
+import Weather from './components/Weather'
 
-export default function App() {
-  return (
-    <Form />
-  )
+const API_KEY = "b86155febf4e7416656ad26119562ec3"
+
+class App extends Component {
+  getWeather = async () =>{
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Chennai&appid=${API_KEY}&units=metric`)
+    const data = await api_call.json()
+    console.log(data)
+  }
+  render() {
+    return (
+      <div>
+        <Title />
+        <Form />
+        <Weather />
+      </div>
+    )
+  }
 }
+
+export default App
+
