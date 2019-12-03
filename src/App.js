5e46b08c32536640d45681fcca276b0c62830f77
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-03 20:41:36 +0530

    Passing props

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Weather from './components/Weather'
 const API_KEY = "b86155febf4e7416656ad26119562ec3"
 
 class App extends Component {
-  getWeather = async () =>{
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Chennai&appid=${API_KEY}&units=metric`)
+  getWeather = async (event) =>{
+    event.preventDefault()
+    const city = event.target.elements.city.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
     const data = await api_call.json()
     console.log(data)
   }
@@ -15,7 +17,7 @@ class App extends Component {
     return (
       <div>
         <Title />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     )
