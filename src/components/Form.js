7da60ea20d4c4e141de55f61e6c34ebaf063538b
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-03 20:41:53 +0530

    Dynamic input from form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 class Form extends Component {
   render() {
     return (
-      <form>
+      <form onSubmit={this.props.getWeather}>
         <input type="text" name="city" placeholder="City..."/>
         {/* <input type="text"/> */}
+        <button>Get weather</button>
       </form>
     )
   }
