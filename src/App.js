3be31b6079dfaeead46b89c722a646bf75eb4e95
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-03 20:56:42 +0530

    Setting states in passing it

--- src/App.js
+++ src/App.js
@@ -6,19 +6,42 @@ import Weather from './components/Weather'
 const API_KEY = "b86155febf4e7416656ad26119562ec3"
 
 class App extends Component {
+  state ={
+    temperature: undefined,
+    city:undefined,
+    country:undefined,
+    humidity:undefined,
+    description:undefined,
+    error:undefined
+  }
   getWeather = async (event) =>{
     event.preventDefault()
     const city = event.target.elements.city.value;
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
     const data = await api_call.json()
     console.log(data)
+    this.setState({
+      temperature : data.main.temp,
+      city : data.name,
+      country : data.sys.country,
+      humidity : data.main.humidity,
+      description : data.weather[0].description,
+      error: ""
+    })
   }
   render() {
     return (
       <div>
         <Title />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather 
+        temperature={this.state.temperature}
+        city={this.state.city}
+        country={this.state.country}
+        humidity={this.state.humidity}
+        description={this.state.description}
+        error={this.state.error}
+         />
       </div>
     )
   }
