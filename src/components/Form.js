e4c5ae9747bb5a44f70c0e1d15aa2d95b1b591ec
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-03 19:34:14 +0530

    Form component

---
+++ src/components/Form.js
@@ -0,0 +1,43 @@
+import React from "react"
+import axios from "axios"
+
+class Form extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+    //   alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    //   let object = {
+    //       value : this.state.value
+    //   }
+      axios.post('https://api.openweathermap.org/data/2.5/weather?q='+"Chennai"+'&appid=b86155febf4e7416656ad26119562ec3')
+        .then(res=>console.log(res.data))
+        .catch(err=>console.log(err))
+    }
+  
+    render() {
+      return (
+        <div>
+            <form onSubmit={this.handleSubmit}>
+            <label>
+                Name:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+            </form>
+        </div>
+      );
+    }
+  }
+
+  export default Form;
