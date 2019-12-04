97adb7892a74c0adb5a013e6a712e04d9b961704
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-04 12:54:45 +0530

    Adding css classnames to elements

--- src/App.js
+++ src/App.js
@@ -52,20 +52,33 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Title />
-        <Form getWeather={this.getWeather} />
-        <Weather 
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        humidity={this.state.humidity}
-        description={this.state.description}
-        errors={this.state.errors}
-         />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Title />
+                </div>
+                <div className="col-xs-7 form-container">
+                <Form getWeather={this.getWeather} />
+                <Weather 
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  humidity={this.state.humidity}
+                  description={this.state.description}
+                  errors={this.state.errors}
+                />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 export default App
+
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Title = () => (
     <div>
-        <h1>WeatherApp</h1>
-        <p>Find out weather conditions anywhere...</p>
+        <h1 className="title-container__title">WeatherApp</h1>
+        <p className="title-container__subtitle">Find out weather conditions of any location...</p>
     </div>
 )
 
