7bdd3081759519589765c152e60aec6f98edf5c6
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-04 11:47:13 +0530

    Single component return (ES6)

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,13 @@
 import React from 'react'
 
-const Weather = (props) =>{
-    return (
-        <div>
-            {props.city && props.country &&<p> Location: {props.city}, {props.country}</p>}
-            {props.temperature && <p>Temperature: {props.temperature}</p>}
-            {props.humidity && <p>Humidity: {props.humidity}</p>} 
-            {props.description && <p>Conditions: {props.description}</p>}
-            {props.errors && <p>{props.errors}</p>}
-        </div>
-    )
-}
+const Weather = props =>(
+    
+    <div>
+        {props.city && props.country &&<p> Location: {props.city}, {props.country}</p>}
+        {props.temperature && <p>Temperature: {props.temperature}</p>}
+        {props.humidity && <p>Humidity: {props.humidity}</p>} 
+        {props.description && <p>Conditions: {props.description}</p>}
+        {props.errors && <p>{props.errors}</p>}
+    </div>
+)
 export default Weather
