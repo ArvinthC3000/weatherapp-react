9c41a35d0962f38e301d88c7bbc74306f38ebbae
ArvinthC3000 <54614142+ArvinthC3000@users.noreply.github.com>
2019-12-04 11:50:57 +0530

    Functional components

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react'
 
-class Title extends Component {
-    render() {
-        return (
-            <div>
-                <h1>WeatherApp</h1>
-                <p>Find out weather conditions anywhere...</p>
-            </div>
-        )
-    }
-}
+const Title = () => (
+    <div>
+        <h1>WeatherApp</h1>
+        <p>Find out weather conditions anywhere...</p>
+    </div>
+)
 
 export default Title
